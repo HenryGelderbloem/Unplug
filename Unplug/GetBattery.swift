27e28ae708c77a024368cbab7ce8a69685aa8553
Henry Gelderbloem <hgelderbloem@icloud.com>
2022-06-21 18:52:07 +0100

    Fixed bug

--- Unplug/GetBattery.swift
+++ Unplug/GetBattery.swift
@@ -103,7 +103,7 @@ public class InternalFinder {
     }
 
     fileprivate func open() {
-        self.serviceInternal = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("AppleSmartBattery"))
+        self.serviceInternal = IOServiceGetMatchingService(kIOMainPortDefault, IOServiceMatching("AppleSmartBattery"))
     }
 
     fileprivate func close() {
